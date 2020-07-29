6d12dff8a831ca93f1c9070d3f276d4e2826966c
teknofage <teknofage@gmail.com>
2020-07-29 15:44:03 -0700

    meddling with Mongo databases and connections

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost', { useNewUr
   if (err) {
     throw err;
   }
+//   ?authSource=admin
 
-
-  const db = client.db('lscn-db');
+  const db = client.db('heroku_c2wdfmns');
   const users = db.collection('users');
   const profiles = db.collection('profiles');
   app.locals.users = users;
